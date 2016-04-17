2963aba0abe9f313ae246711f7fb5e5d32db09c5
kmclachlan <kaitlyn2004@gmail.com>
2016-04-17 11:52:32 -0400

    Change format of 500px environment variables

--- lib/config.inc.php
+++ lib/config.inc.php
@@ -3,7 +3,7 @@
 define('FLICKR_API_KEY', getenv('FLICKR_API_KEY'));
 define('FLICKR_API_SECRET', getenv('FLICKR_API_SECRET'));
 
-define('FIVEHUNDREDPX_API_KEY', getenv('500PX_API_KEY'));
-define('FIVEHUNDREDPX_API_SECRET', getenv('500PX_API_SECRET'));
+define('FIVEHUNDREDPX_API_KEY', getenv('PX500_API_KEY'));
+define('FIVEHUNDREDPX_API_SECRET', getenv('PX500_API_SECRET'));
 
 ?>
